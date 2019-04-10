7359ce011eecda8058a9f0498cf918ad8d4bf249
Zach Zhou <ispmd@foxmail.com>
2019-04-10 17:08:45 +0800

    add DateUtil

---
+++ src/main/java/date/DateUtil.java
@@ -0,0 +1,38 @@
+package date;
+
+import org.joda.time.DateTime;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DateUtil {
+
+    public static List<String> getBetweenDate(String start, String end) {
+        List<String> list = new ArrayList<>();
+        LocalDate startDate = LocalDate.parse(new DateTime(start).toString("yyyy-MM-dd"));
+        LocalDate endDate = LocalDate.parse(new DateTime(end).toString("yyyy-MM-dd"));
+
+        long distance = ChronoUnit.DAYS.between(startDate, endDate);
+        if (distance < 1) {
+            return list;
+        }
+        Stream.iterate(startDate, d -> d.plusDays(1))
+                .limit(distance + 1)
+                .forEach(d -> list.add(d.toString()));
+        return list;
+    }
+
+
+    public static String FormatDate(String date) {
+        return new DateTime(date).toString("yyyy-MM-dd");
+    }
+
+    public static void main(String[] args) {
+
+        getBetweenDate("2018-01-25", "2018-01-29").forEach(System.out::println);
+    }
+
+}
