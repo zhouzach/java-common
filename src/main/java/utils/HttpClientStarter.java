0c7d6e2637545dc44592c0aead1fe4ed3ecd6960
Zhou Zach <wander669@gmail.com>
2019-05-22 18:56:17 +0800

    add okhttp3 client

---
+++ src/main/java/utils/HttpClientStarter.java
@@ -0,0 +1,74 @@
+package utils;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * handle requestBody
+ * https://www.programcreek.com/java-api-examples/?api=com.squareup.okhttp.ResponseBody
+ */
+public class HttpClientStarter {
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+    private static OkHttpClient buildeClient(long connectTimeout, long readTimeout, long writeTimeout) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(connectTimeout, TimeUnit.SECONDS);
+        builder.readTimeout(readTimeout, TimeUnit.SECONDS);
+        builder.writeTimeout(writeTimeout, TimeUnit.SECONDS);
+        return builder.build();
+    }
+
+    public static String post(String url, RequestBody body) {
+        return post(url, body, 60 * 60 * 24, 60 * 60 * 24, 60 * 60 * 24);
+    }
+
+    public static String post(String url, RequestBody body, long connectTimeout, long readTimeout, long writeTimeout) {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = buildeClient(connectTimeout, readTimeout, writeTimeout).newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        HttpClientStarter starter = new HttpClientStarter();
+
+        String postUrl = "";
+
+        JSONObject postJson = new JSONObject();
+        postJson.put("begin", "");
+        postJson.put("end", "");
+        RequestBody body = RequestBody.create(JSON, postJson.toString());
+
+        String response = post(postUrl, body);
+        System.out.println(response);
+
+        String url = "";
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("PARAM", "");
+        jsonObject.put("VALUE", "");
+        jsonObject.put("OPERATOR", "=");
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(jsonObject);
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("", "")
+                .add("", jsonArray.toString())
+                .build();
+
+        String response1 = post(url, formBody);
+        System.out.println(response1);
+    }
+}
