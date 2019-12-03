9454e61653080a953c0811bea0987aa4450935ef
Zach Zhou <769087026@qq.com>
2019-12-03 17:08:46 +0800

    list suports group by multiKeys and sort

--- src/main/java/consts/Number.java
+++ src/main/java/consts/Number.java
@@ -2,6 +2,8 @@ package consts;
 
 import java.lang.annotation.Native;
 import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Number {
     /**
@@ -12,13 +14,34 @@ public class Number {
     public static final int   MAX_VALUE = 0x7fffffff;
 
 
-    public static void main(String[] args){
-
+    public static void max(final String str) {
         double dd = Number.MAX_VALUE / 4;
         System.out.println(dd);
         BigDecimal d = BigDecimal.valueOf(Number.MAX_VALUE / 4);
 
         System.out.println(d);
+    }
 
+    public static void main(String[] args){
+
+        boolean b =isNumeric("2000000000000");
+        System.out.println(b);
+    }
+
+    public static boolean isNumeric(final String str) {
+        // 该正则表达式可以匹配所有的数字 包括负数
+        Pattern pattern = Pattern.compile("-?[0-9]+(\\.[0-9]+)?");
+        String bigStr;
+        try {
+            bigStr = new BigDecimal(str).toString();
+        } catch (Exception e) {
+            return false;//异常 说明包含非数字。
+        }
+
+        Matcher isNum = pattern.matcher(bigStr); // matcher是全匹配
+        if (!isNum.matches()) {
+            return false;
+        }
+        return true;
     }
 }
