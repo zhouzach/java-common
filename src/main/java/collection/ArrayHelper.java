93a782faecf4e631a7a285a8726f1769d333a7a5
Zach Zhou <769087026@qq.com>
2019-12-04 11:19:42 +0800

    add array copy

--- src/main/java/collection/ArrayHelper.java
+++ src/main/java/collection/ArrayHelper.java
@@ -4,18 +4,33 @@ import java.util.Arrays;
 
 public class ArrayHelper {
 
-    public static void print(String[] array){
+    public static void print(String[] array) {
         System.out.println(Arrays.toString(array));
     }
 
 
-    public static String[] init(){
-        String array[] = new String[] {
-                "Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda" };
+    public static String[] init1() {
+        String[] array = {"Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda"};
         return array;
     }
-    public static void main(String[] args){
 
-        print(init());
+    public static String[] init2() {
+        String array[] = new String[]{
+                "Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda"};
+        return array;
+    }
+
+    public static String[] copy(String[] a) {
+        String[] b = new String[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        print(init1());
+
+        print(copy(init1()));
     }
 }
